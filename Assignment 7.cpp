9e858a2bea0486e3e18dc7c0fd97e33f11ed2a80
ridhii3579 <rmahajan_be24@thapar.edu>
2025-11-11 14:50:44 +0530

    Assignment 7.cpp

--- Assignment 7.cpp
+++ Assignment 7.cpp
@@ -1,1 +1,246 @@
+// 1
+#include <iostream>
+using namespace std;
 
+void selectionSort(int arr[], int n){
+    for(int i = 0; i < n - 1; i++){
+        int minIndex = i;
+        for(int j = i + 1; j < n; j++){
+            if(arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+void insertionSort(int arr[], int n){
+    for(int i = 1; i < n; i++){
+        int key = arr[i];
+        int j = i - 1;
+        while(j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void bubbleSort(int arr[], int n){
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; j < n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+void merge(int arr[], int l, int m, int r){
+    int n1 = m-l+1;
+    int n2 = r-m;
+    int L[n1], R[n2];
+
+    for(int i=0; i<n1; i++) L[i] = arr[l+i];
+    for(int i=0; i<n2; i++) R[i] = arr[m+1+i];
+
+    int i=0, j=0, k=l;
+    while(i<n1 && j<n2){
+        if(L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
+
+    while(i<n1) arr[k++] = L[i++];
+    while(j<n2) arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int l, int r){
+    if(l < r){
+        int m = (l+r)/2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+int partitionFunc(int arr[], int low, int high){
+    int pivot = arr[high];
+    int i = low - 1;
+    for(int j = low; j < high; j++){
+        if(arr[j] < pivot){
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i+1], arr[high]);
+    return i+1;
+}
+
+void quickSort(int arr[], int low, int high){
+    if(low < high){
+        int pi = partitionFunc(arr, low, high);
+        quickSort(arr, low, pi-1);
+        quickSort(arr, pi+1, high);
+    }
+}
+
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main(){
+    int n, choice;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for(int i=0; i<n; i++) cin >> arr[i];
+
+    cout << "\nSorting Menu\n";
+    cout << "1. Selection Sort\n";
+    cout << "2. Insertion Sort\n";
+    cout << "3. Bubble Sort\n";
+    cout << "4. Merge Sort\n";
+    cout << "5. Quick Sort\n";
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch(choice){
+        case 1: selectionSort(arr, n); break;
+        case 2: insertionSort(arr, n); break;
+        case 3: bubbleSort(arr, n); break;
+        case 4: mergeSort(arr, 0, n-1); break;
+        case 5: quickSort(arr, 0, n-1); break;
+        default: cout << "Invalid Choice"; return 0;
+    }
+
+    cout << "\nSorted Array: ";
+    printArray(arr, n);
+    return 0;
+}
+
+
+// 2
+#include <iostream>
+using namespace std;
+
+void improvedSelectionSort(int arr[], int n)
+{
+    int start = 0, end = n - 1;
+    while (start < end)
+    {
+        int minIndex = start;
+        int maxIndex = start;
+        for (int i = start; i <= end; i++)
+        {
+            if (arr[i] < arr[minIndex])
+                minIndex = i;
+            if (arr[i] > arr[maxIndex])
+                maxIndex = i;
+        }
+        swap(arr[start], arr[minIndex]);
+        if (maxIndex == start)
+            maxIndex = minIndex;
+        swap(arr[end], arr[maxIndex]);
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    improvedSelectionSort(arr, n);
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
+
+// Additional Questions 1
+#include <iostream>
+using namespace std;
+
+void countingSort(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
+    int count[max + 1] = {0};
+    for (int i = 0; i < n; i++)
+        count[arr[i]]++;
+    for (int i = 1; i <= max; i++)
+        count[i] += count[i - 1];
+    int output[n];
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements (non-negative): ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    countingSort(arr, n);
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
+
+// Additional Questions 2
+#include <iostream>
+using namespace std;
+
+void countingSort(int arr[], int n, int exp) {
+    int output[n];  
+    int count[10] = {0};\
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+void radixSort(int arr[], int n) {
+    int maxVal = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > maxVal)
+            maxVal = arr[i];
+    for (int exp = 1; maxVal / exp > 0; exp *= 10)
+        countingSort(arr, n, exp);
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    radixSort(arr, n);
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
